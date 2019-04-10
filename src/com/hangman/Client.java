e45ee4818f3d17309d3c4f49cb8332f0bd351e80
Ronnachai <ronnachai45399@gmail.com>
2019-04-10 13:55:50 +0700

    add Client, Server class

---
+++ src/com/hangman/Client.java
@@ -0,0 +1,9 @@
+package com.hangman;
+
+public class Client {
+    private int PORT = 8888;
+
+    public static void main(String[] args) {
+        System.out.println("Hello Client");
+    }
+}
---
+++ src/com/hangman/Server.java
@@ -0,0 +1,9 @@
+package com.hangman;
+
+public class Server {
+    private int PORT = 8888;
+
+    public static void main(String[] args) {
+        System.out.println("Hello Server");
+    }
+}
