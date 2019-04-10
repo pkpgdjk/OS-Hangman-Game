96eac034c8bb40599c91c423d87ad25a9b4b0333
Ronnachai <ronnachai45399@gmail.com>
2019-04-10 14:04:43 +0700

    edit client

--- src/com/hangman/Client.java
+++ src/com/hangman/Client.java
@@ -4,6 +4,6 @@ public class Client {
     private int PORT = 8888;
 
     public static void main(String[] args) {
-        System.out.println("Hello Client test");
+        System.out.println("Hello Client asdasdasdasdasdasdasdasdasd");
     }
 }
