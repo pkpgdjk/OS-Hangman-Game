ea2b6eba06306d218f66b364fc7086445c3c53cb
ayna <hana_phu@hotmail.com>
2019-04-26 22:40:35 +0700

    Initial Port in Class Server

--- src/com/hangman/Server.java
+++ src/com/hangman/Server.java
@@ -1,7 +1,12 @@
 package com.hangman;
 
+import java.net.ServerSocket;
+
 public class Server {
-    private int PORT = 8888;
+    //socket server port on which it will listen
+    private static int PORT = 8888;
+    //static ServerSocket variable
+    private static ServerSocket server;
 
     public static void main(String[] args) {
         System.out.println("Hello Server");
