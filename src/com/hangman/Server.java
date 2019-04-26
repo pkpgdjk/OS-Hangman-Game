ecdbd04711e661b3e88973cc1d88e86e08f310d5
ayna <hana_phu@hotmail.com>
2019-04-26 22:57:55 +0700

    Create new ServerSocket in Class Server

--- src/com/hangman/Server.java
+++ src/com/hangman/Server.java
@@ -1,6 +1,8 @@
 package com.hangman;
 
+import java.io.IOException;
 import java.net.ServerSocket;
+import java.lang.ClassNotFoundException;
 
 public class Server {
     //socket server port on which it will listen
@@ -8,7 +10,21 @@ public class Server {
     //static ServerSocket variable
     private static ServerSocket server;
 
-    public static void main(String[] args) {
-        System.out.println("Hello Server");
+    public static void main(String[] args) throws ClassNotFoundException {
+        int i =0;
+        try {
+            server = new ServerSocket(PORT);
+            while (true) {
+                System.out.println("Waiting for the client request");
+
+            }
+        }
+        catch (IOException ex) {
+            try {
+                server.close();
+            } catch (IOException e) {
+                System.err.println("ERROR closing socket: " + e.getMessage());
+            }
+        }
     }
 }
