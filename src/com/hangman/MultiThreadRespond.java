f86e43cd4393bda49f9c8036edaeecced2ddf996
Kawinna Yotapayap <ked44839@hotmail.com>
2019-04-27 15:53:28 +0700

    add

--- src/com/hangman/MultiThreadRespond.java
+++ src/com/hangman/MultiThreadRespond.java
@@ -1,8 +1,10 @@
 package com.hangman;
 
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class MultiThreadRespond implements Runnable{
     private ServerSocket server;
@@ -24,6 +26,11 @@ public class MultiThreadRespond implements Runnable{
     public void run() {
         while(true){
             try{
+                Socket socket = server.accept();
+                //read from socket to ObjectInputStream object
+                ois = new ObjectInputStream(socket.getInputStream());
+                //convert ObjectInputStream object to String
+                String message = (String) ois.readObject();
 
             }catch(Exception e){
 
