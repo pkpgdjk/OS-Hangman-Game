bbd44ed9b9897a5e7d864a29af5e4f9c74a49648
pkpgdjk <pkpgdjk@gmail.com>
2019-05-05 22:00:11 +0700

    Update script-server.sh

--- script-server.sh
+++ script-server.sh
@@ -1,2 +1,14 @@
-docker build -f Dockerfile-server -t hangman-server-image .
-docker run -dit --name hangman-server hangman-server-image
+#!/bin/bash
+CONTAINER_NAME="hangman-server"
+IMAGE_NAME="hangman-server-image"
+DOCKERFILE_NAME="Dockerfile-server"
+
+
+ECHO "===> REMOVE CONTAINERS <===="
+echo "removing $CONTAINER_NAME"
+docker rm -f $CONTAINER_NAME
+ECHO "===> BUILD DOCKER IMAGE <===="
+docker build -f $DOCKERFILE_NAME -t $IMAGE_NAME .
+
+echo "===> RUN DOCKER CONTAINER <==="
+docker run -dit --name $CONTAINER_NAME $IMAGE_NAME
