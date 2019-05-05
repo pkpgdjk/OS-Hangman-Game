26a0216d0edbd06bbccc7e8d8e81230c63a0e1f1
pkpgdjk <pkpgdjk@gmail.com>
2019-05-05 21:58:14 +0700

    Update script-client.sh

--- script-client.sh
+++ script-client.sh
@@ -1,2 +1,14 @@
-docker build -f Dockerfile-client -t hangman-client-image .
-docker run -dit --name hangman-client --link hangman-server:server hangman-client-image
+#!/bin/bash
+CONTAINER_NAME="hangman-client"
+IMAGE_NAME="hangman-client-image"
+DOCKERFILE_NAME="Dockerfile-client"
+
+
+ECHO "===> REMOVE CONTAINERS <===="
+echo "removing $CONTAINER_NAME"
+docker rm -f $CONTAINER_NAME
+ECHO "===> BUILD DOCKER IMAGE <===="
+docker build -f $DOCKERFILE_NAME -t $IMAGE_NAME .
+
+echo "RUN THIS COMMAND"
+echo "docker run -dit --name $CONTAINER_NAME --link hangman-server:server $IMAGE_NAME"
