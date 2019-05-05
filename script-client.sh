a2960f712922f267dbb1321f20f1968851694ba5
pkpgdjk <pkpgdjk@gmail.com>
2019-05-05 21:47:42 +0700

    Update script-client.sh

--- script-client.sh
+++ script-client.sh
@@ -1,2 +1,2 @@
-docker build -f Dockerfile-client -t hangman-client-image
+docker build -f Dockerfile-client -t hangman-client-image .
 docker run -dit --name hangman-client --link hangman-server:server hangman-client-image
